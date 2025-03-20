1112826f4788fd0352104934534f5e8afbe669d5
cracker38 <it.elias38@gmail.com>
2025-03-20 16:40:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,18 @@ if ($text == "") {
     $response .= "2. Phone Number";
 } elseif ($text == "1") {
     $response = "CON Choose depending on what you want:\n";
-    $response .= "1.1 Account Code Number\n";
-    $response .= "1.2 Account Balance";
+    $response .= "1*1 Account Code Number\n";
+    $response .= "1*2 Account Balance";
 } elseif ($text == "2") {
     $response = "END My phone number is " . $phonenumber;
-} elseif ($text == "1*1") { 
+} elseif ($text == "1.1" || $text == "1*1") { 
     $accountcode = "CC101";
     $response = "END My Account Code: " . $accountcode;
-} elseif ($text == "1*2") { 
+} elseif ($text == "1.2" || $text == "1*2") { 
     $balance = "RWF 1000";
     $response = "END My Account Balance: " . $balance;
+} else {
+    $response = "END Invalid option. Please try again.";
 }
 
 header("Content-Type: text/plain");
