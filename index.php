b794a124a5df9c45ab73a0148289135a7afd4f13
cracker38 <it.elias38@gmail.com>
2025-03-20 16:28:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,29 @@
 <?php
 
-$sessionid=$_POST['sessionid'];
-$servicecode=$_POST['servicecode'];
-$phonenumber=$_POST['phonenumber'];
-$text=$_POST['text'];
+$sessionid = $_POST['sessionid'] ?? '';
+$servicecode = $_POST['servicecode'] ?? '';
+$phonenumber = $_POST['phonenumber'] ?? '';
+$text = $_POST['text'] ?? '';
 
-if($text=="")
-{
-
-    $response="CON check what you want to choose:\n";
-    $response.="1. Account No";
-    $response.="2. phone Number";
-}else if($text=="1"){
-    $response="Choose depend on what you want:\n";
-    $response.="1. Account Code Number";
-    $response.="2. Account Balance";
-}else if($text=="2"){
-    $response="End my phone number ".$phonenumber;
-    exit();
-}else if($text=="1.1"){
-    $accountcode="CC101";
-    $response="End My Account code ".$accountcode;
-    exit();
-}else if($text=="1.2"){
-    $balance="RFW 1000";
-    $response="End My Account balance ".$balance;
+if ($text == "") {
+    $response = "CON Check what you want to choose:\n";
+    $response .= "1. Account No\n";
+    $response .= "2. Phone Number";
+} elseif ($text == "1") {
+    $response = "CON Choose depending on what you want:\n";
+    $response .= "1.1 Account Code Number\n";
+    $response .= "1.2 Account Balance";
+} elseif ($text == "2") {
+    $response = "END My phone number is " . $phonenumber;
+} elseif ($text == "1*1") { 
+    $accountcode = "CC101";
+    $response = "END My Account Code: " . $accountcode;
+} elseif ($text == "1*2") { 
+    $balance = "RWF 1000";
+    $response = "END My Account Balance: " . $balance;
 }
 
-
-header("Content-Type: application/json");
+header("Content-Type: text/plain");
 echo $response;
+
 ?>
